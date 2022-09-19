593a0af7ebdb25eb4081df513b49ac52096a9d77
Misty Becker <mistyb01@uw.edu>
2022-09-18 18:51:05 -0700

    fixed menu bug, added svg

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const header = document.querySelector('header');
 const toggleButton = header.querySelector('.toggleBtn');
+const menu = header.querySelector('.menu');
+
 toggleButton.addEventListener('click', toggleMenu);
 
+window.onresize = retoggle;
+function retoggle() {
+    if (window.innerWidth > 768) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
 
 let isToggled = false;
-function toggleMenu(e) {
+function toggleMenu() {
     isToggled = !isToggled;
-    const menu = header.querySelector('.menu');
     if (isToggled) {
         menu.style.display ='block';
     } else {
         menu.style.display = 'none';
     }
 }
+
